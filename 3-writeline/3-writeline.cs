5b7484f912386009cd095028ce0cde0c83239908
hczde <hczde@vk.ru>
2025-04-23 19:42:01 +0400

    ss

---
+++ 3-writeline/3-writeline.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+
+        }
+    }
+}
