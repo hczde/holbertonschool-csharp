3039329d66e4aa8259cec427579ca36a772e2847
hczde <hczde@vk.ru>
2025-04-23 20:03:46 +0400

    ss

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-
-namespace _4_print_integer
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             int number = 11;
-            Console.WriteLine(number);
         }
     }
 }
