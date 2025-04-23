af9871697fbe5a3e65cb28d68633639e9ef11bb7
hczde <hczde@vk.ru>
2025-04-23 20:28:37 +0400

    ss

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        float number = 3.14f;
-        Console.WriteLine("Float: " + number);
+        float number = 3.14159f;
+        Console.WriteLine("Float: " + number.ToString("F2"));
     }
 }
