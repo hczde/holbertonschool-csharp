7f80d465a96a1918c80d9b3273b330c6ce66ab30
hczde <hczde@vk.ru>
2025-04-24 16:37:17 +0400

    ss

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		Console.WriteLine(str.Substring(0,9));str.sub
+		Console.WriteLine(str.Substring(0,9));
     }
 }
