d6d34348b12690736f626aa36e2de849e56e52b5
hczde <hczde@vk.ru>
2025-04-24 17:18:29 +0400

    s

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
 		double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0:0.0%}",percent);
-        Console.WriteLine("Currency: ${0:N2}",currency);
+        Console.WriteLine("Currency: {0:C2}",currency);
 	}
 }
