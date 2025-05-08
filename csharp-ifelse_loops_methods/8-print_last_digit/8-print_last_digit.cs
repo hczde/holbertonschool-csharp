6c72e8589c612118eb5f997938860165aaad1ab7
hczde <hczde@vk.ru>
2025-05-08 12:22:08 -0300

    sa

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {  public static int PrintLastDigit(int number)
+    {
+        int x = number % 10;
+        if (x < 0)
+            x = x * -1;
+        Console.Write(x);
+        return x; 
+    }
+
+
+    }
+}
