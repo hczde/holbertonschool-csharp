5db67cab9533b5aa1506023f45d69ba332eba633
hczde <hczde@vk.ru>
2025-05-18 10:49:59 -0300

    sa

--- csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -1,21 +1,29 @@
-﻿for (int i = 1; i <= 100;i++)
+﻿using System;
+
+class Program
 {
-	if (i % 3 == 0 & i % 5 == 0)
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write("FizzBuzz ");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.Write("Fizz ");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write("Buzz ");
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
+        }
 
-	{
-		Console.Write("FizzBuzz");
-	}
-	else if (i % 3 == 0)
-	{
-		Console.Write("Fizz");
-	}
-	else if (i % 5 == 0)
-	{
-		Console.Write("Buzz");
-
-	}
-	else
-	{
-		Console.Write(i + " ");
-	}
+        
+    }
 }
